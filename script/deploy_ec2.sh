4c8d454556d773c3cfcbba1bcc7f902ef82890f8
runes224 <hitakarento@gmail.com>
2019-10-20 17:45:27 +0900

    Add function Allow access to EC2 only run circleci

---
+++ script/deploy_ec2.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
+export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
+export AWS_DEFAULT_REGION="us-east-2a"
+
+MYSECURITYGROUP=”sg-0a37eeb37fb44ccee"
+MYIP=`curl -s ifconfig.me`
+
+aws ec2 authorize-security-group-ingress --group-id $MYSECURITYGROUP --protocol tcp --port 22 --cidr $MYIP/32
+ssh ${USER_NAME}@${HOST_NAME} 'cd /var/www/rails/teamlab_selection_task && git pull && docker-compose build && docker-compose run web bundle install && docker-compose restart'
+aws ec2 revoke-security-group-ingress --group-id $MYSECURITYGROUP --protocol tcp --port 22 --cidr $MYIP/32
