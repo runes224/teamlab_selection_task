c01a6c73bf81e0b02477cc9f14d84dba9455ae23
runes224 <hitakarento@gmail.com>
2019-10-22 20:47:22 +0900

    Add comments

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,15 @@
+#ベースイメージの指定
 FROM ruby:2.6.5-alpine3.10
+
+# 環境変数の指定
 ENV LANG C.UTF-8
+
+# パッケージのアップグレード＆追加
 RUN apk upgrade && \
     apk add --no-cache \
     bash \
     build-base \
     curl-dev \
-#    バージョン管理
     git \
     graphviz \
     imagemagick \
@@ -29,19 +33,33 @@ RUN apk upgrade && \
     zlib \
     zlib-dev
 
+# bundlerのインストール
 RUN gem install bundler
+
+# インストール済み全gemを更新
 RUN gem update
 
+# アプリケーションのディレクトリを定義する
 ENV APP_HOME /teamlab_selection_task
+
+# アプリケーションのルートディレクトリを作成する
 RUN mkdir $APP_HOME
+
+# ワークディレクトリを設定する
 WORKDIR $APP_HOME
 
+# Gem のホームディレクトリを定義する
 ENV GEM_HOME=/bundle
 
+# アプリケーションのディレクトリにGemfileを追加
 ADD Gemfile* $APP_HOME/
 
+# Bundlerで使用するGemfileの定義
 ENV BUNDLE_GEMFILE=$APP_HOME/Gemfile \
-  BUNDLE_JOBS=4 \
-  BUNDLE_PATH=$GEM_HOME
+    # Bundlerの並列実行可能な数を定義する
+    BUNDLE_JOBS=4 \
+    # bundlerのパスを指定する
+    BUNDLE_PATH=$GEM_HOME
 
+# アプリケーションをルートディレクトリに追加する
 ADD . $APP_HOME
--- script/deploy_ec2.sh
+++ script/deploy_ec2.sh
@@ -3,10 +3,14 @@
 export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
 export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
 export AWS_DEFAULT_REGION="us-east-2"
-
+# EC2が属するSGを定義する
 MYSECURITYGROUP="sg-0a37eeb37fb44ccee"
+# 自身のグローバルIPアドレスを定義する
 MYIP=`curl -s ifconfig.me`
 
+# 一時的に自身のグローバルIPアドレスからEC2が属するSGへのアクセスを許可する
 aws ec2 authorize-security-group-ingress --group-id $MYSECURITYGROUP --protocol tcp --port 22 --cidr $MYIP/32
-ssh ${USER_NAME}@${HOST_NAME} 'cd /var/www/rails/teamlab_selection_task && git pull && docker-compose build && docker-compose run web rails db:migrate && docker-compose run web bundle install && docker-compose restart'
+# EC2にログインして、デプロイする
+ssh ${USER_NAME}@${HOST_NAME} 'cd /var/www/rails/teamlab_selection_task && git pull && docker-compose build && docker-compose run web bundle exec rails db:migrate && docker-compose run web bundle install && docker-compose restart'
+# 自身のグローバルIPアドレスからEC2が属するSGへのアクセス許可を取り消す
 aws ec2 revoke-security-group-ingress --group-id $MYSECURITYGROUP --protocol tcp --port 22 --cidr $MYIP/32
